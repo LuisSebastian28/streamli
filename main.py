4e460d355bb762b8acda94b75a2fff38433cb46b
ricardo051199 <jesus.pelaez@ucb.edu.bo>
2024-05-04 17:13:12 -0400

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import streamlit as st
+import time
 
-with st.chat_message("user"):
-    st.write("hola mundo")
+def simulate_loading(email, password):
+    with st.spinner("Autenticando..."):
+        time.sleep(5)
+        st.success("¡Autenticación exitosa!")
+
+def main():
+    st.title("Página de Carga en Streamlit")
+
+    email = st.text_input("Correo electrónico:")
+    password = st.text_input("Contraseña:", type="password")
+
+    if st.button("Iniciar carga"):
+        if email and password:
+            simulate_loading(email, password)
+        else:
+            st.warning("Por favor, ingrese su correo electrónico y contraseña.")
+
+if __name__ == "__main__":
+    main()
